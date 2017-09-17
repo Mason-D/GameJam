8977d7b4807438ee471c42f9561b7f77f6c63117
Logan Davis <wowlogan22@tamu.edu>
2017-09-16 21:47:31 -0500

    add thread sleep

--- src/team/ljm/game/Game.java
+++ src/team/ljm/game/Game.java
@@ -46,6 +46,12 @@ public class Game {
 		case PAUSED:
 			if (Keyboard.isKeyDown(Keyboard.KEY_ESCAPE))
 				setGameState(GameState.MENU);
+			try {
+				Thread.sleep(200);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			break;
 
 		}
